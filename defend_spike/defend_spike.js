f6842a3b2decb069187538634c2375ccbee9bb9c
Artem-Lazariev <lazarievartem25@gmail.com>
2026-04-07 09:41:36 +0300

    Fix spikeExplosion function to use center parameter

--- defend_spike/defend_spike.js
+++ defend_spike/defend_spike.js
@@ -290,7 +290,7 @@ document.getElementById("rtp").addEventListener("click", function () {
     }
 })
 function spikeExplosion(center) {
-    let player2 = {...spike}; //or spike
+    let player2 = {...center};
     let p1 = {x: player2.x - 1, y: player2.y}
     let p2 = {x: player2.x + 1, y: player2.y}
     let p3 = {x: player2.x, y: player2.y - 1}
@@ -326,7 +326,6 @@ function spikeExplosion(center) {
         ,500)
 
 }
-
 document.getElementById("spike").addEventListener("click", function () {
 
     if (i >= 30 && !sp) {
